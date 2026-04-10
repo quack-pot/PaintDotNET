0cd85c521f19a3909f60921019c523d1c14d2d06
Quack Pot <skyler32702@gmail.com>
2026-04-10 15:42:07 -0500

    Minor update to avoid players resetting their paint timer over fully-painted tiles.

--- Backend/PaintDotNET.Core/Systems/PaintSystem.cs
+++ Backend/PaintDotNET.Core/Systems/PaintSystem.cs
@@ -21,8 +21,6 @@ public class PaintSystem(GameState injected_game_state, ItemsStore<Player> injec
                 continue;
             }
 
-            player.paint_cooldown_secs = GameRules.PLAYER_PAINT_COOLDOWN_SECS;
-
             int grid_x = (int)MathF.Floor(player.position.X);
             int grid_y = (int)MathF.Floor(player.position.Y);
 
@@ -32,15 +30,15 @@ public class PaintSystem(GameState injected_game_state, ItemsStore<Player> injec
 
             ref Tile tile = ref game_state.GetTile(grid_x, grid_y);
             
-            if (tile.team == player.team)
+            if (tile.team != player.team)
             {
-                tile.team = player.team;
-                tile.strength = MathGen.Min(tile.strength + 1, GameRules.MAX_PAINT_STRENGTH);
-                continue;
+                tile.strength--;
+                player.paint_cooldown_secs = GameRules.PLAYER_PAINT_COOLDOWN_SECS;
             }
-            else
+            else if (tile.strength < GameRules.MAX_PAINT_STRENGTH)
             {
-                tile.strength--;    
+                tile.strength++;
+                player.paint_cooldown_secs = GameRules.PLAYER_PAINT_COOLDOWN_SECS;
             }
 
             if (tile.strength <= 0)
